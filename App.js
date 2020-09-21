006460c5ff7ba9f3113a3880859f0ef274b3d2a4
Deepak1jha <ddjha171@gmail.com>
2020-09-21 21:59:07 +0530

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,SafeAreaView } from 'react-native';
+import { AntDesign,Entypo,Ionicons } from '@expo/vector-icons';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <SafeAreaView style={{alignSelf:"stretch"}}>
+        <View style={{marginHorizontal:32 ,marginTop:32}}>
+          <View style={styles.topContainer}>
+          <AntDesign name={"arrowleft"} size={33}/>
+          </View>
+        </View>
+
+      </SafeAreaView>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
@@ -14,8 +21,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#dee9fd',
     alignItems: 'center',
-    justifyContent: 'center',
+    // justifyContent: 'center',
   },
+  topContainer:{
+
+  }
 });
---
+++ src/colors/Colors.js
@@ -0,0 +1,11 @@
+export default {
+  gray: "#91A1BD",
+  primary: "#fc5c65",
+  secondary: "#4ecdc4",
+  black: "#000",
+  white: "#fff",
+  mediumGrey: "#6e6969",
+  lightGrey: "#f8f4f4",
+  danger: "#ff5252",
+  dark: "#0c0c0c"
+}
